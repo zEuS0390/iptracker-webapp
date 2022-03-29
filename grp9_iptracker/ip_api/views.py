21c8e80c0847c24a467ff01ab7ece342b4640b36
zjsbaltazar-tip <qzjsbaltazar@tip.edu.ph>
2022-03-29 18:08:01 +0800

    Add functionality

--- grp9_iptracker/ip_api/views.py
+++ grp9_iptracker/ip_api/views.py
@@ -14,4 +14,8 @@ class Index(View):
     def post(self, request):
         ip_address = request.POST.get("ip_address")
         data = getIPInfo(ip_address)
+        if len(data["languages"]) > 50:
+            data["languages"] = data["languages"][:50] + "\n" + data["languages"][50:]
+        if len(data["org"]) > 50:
+            data["org"] = data["org"][:50] + "\n" + data["org"][50:]
         return render(request, template_name="ip_api/result.html", context={"data": data})
