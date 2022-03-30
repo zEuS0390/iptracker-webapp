cd879fbfba66dc7a61b8ff3ab96904c191e91e70
crebello26 <crebellocynnamae@gmail.com>
2022-03-31 02:53:52 +0800

    Redesign UI and Implement api

--- grp9_iptracker/ip_api/api.py
+++ grp9_iptracker/ip_api/api.py
@@ -3,6 +3,34 @@ from abc import ABC, abstractmethod
 from decouple import config
 import requests
 
+field = {
+    "ip": "ip_address",
+    "city": "city_name",
+    "region": "region_name",
+    "region_code": "region_code",
+    "country": "country_name",
+    "country_code": "country_code_2letter",
+    "country_code_iso3": "country_code_3ltter",
+    "counrty_capital": "country_capital",
+    "country_tld":"country_tld",
+    "continent_code":"continent_code",
+    "in_eu": False,
+    "postal": "postal_code",
+    "latitude" :"latitude",
+    "longitude" :"longitude",
+    "timezone": "timezone", # format area/location
+    "utc_offset":"UTC_time",
+    "country_calling_code":"calling_code",
+    "currency": "currency_code",
+    "currency_name": "currency_name",
+    "languages":"language",
+    "country_area": "area_country", #in sq km
+    "counrty_population": "number_population",
+    "asn": "autonomous_system_number",
+    "org" : "organization_name",
+    "hostname": "host_name",
+}
+
 class IPAPI(ABC):
     @abstractmethod
     def completeLocation(self): ...
